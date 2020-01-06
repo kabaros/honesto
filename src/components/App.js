db8ec07e67e33914feb99760a3c4ee88786e9052
mozafar <kabaros@gmail.com>
2020-01-06 19:23:33 +0100

    chore: Add Bootstrap CSS framework
    Add Bootstrap and style the navbar with navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -4,23 +4,34 @@ import NavBar from "./NavBar";
 import ShareFeedback from "./ShareFeedback";
 import ViewFeedback from "./ViewFeedback";
 import NoMatch from "./404page";
+
 function App() {
   return (
     <Router>
       <div>
         <NavBar />
-
-        <Switch>
-          <Route exact path="/">
-            <ShareFeedback />
-          </Route>
-          <Route path="/view-feedback/:mode">
-            <ViewFeedback />
-          </Route>
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center ">
+              <Switch>
+                <Route exact path="/">
+                  <ShareFeedback />
+                </Route>
+                <Route path="/view-feedback/:mode">
+                  <ViewFeedback />
+                </Route>
+                <Route path="*">
+                  <NoMatch />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+        </div>
+        <footer className="bg-dark navbar-dark fixed-bottom ">
+          <p className="muted text-right text-light p-2">
+            Time spent so far: 1h
+          </p>
+        </footer>
       </div>
     </Router>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,60 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav
+      className={`${styles.nav} navbar navbar-expand-lg navbar-light static-top`}
+    >
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          Honesto
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarResponsive"
+          aria-controls="navbarResponsive"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink
+                activeClassName={styles.activeLink}
+                className={`${styles.navLink}`}
+                to="/view-feedback/given"
+              >
+                My Feedback
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                activeClassName={styles.activeLink}
+                className={`${styles.navLink}`}
+                to="/view-feedback/received"
+              >
+                Team Feedback
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                activeClassName={styles.activeLink}
+                className={`${styles.navLink}`}
+                to="/teams"
+              >
+                Teams
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      {/* <nav>
       <ul>
         <li>
           <Link to="/">Share Feedback</Link>
@@ -18,6 +69,7 @@ const NavBar = () => {
           <Link to="/teams">Teams</Link>
         </li>
       </ul>
+    </nav>*/}
     </nav>
   );
 };
